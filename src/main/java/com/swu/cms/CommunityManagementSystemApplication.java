7fbfdf9a45b37e136999cbc7e6c510c74a61fe3a
zr-swu <74178622+zr-swu@users.noreply.github.com>
2021-03-15 17:00:42 +0800

    Initial commit

---
+++ src/main/java/com/swu/cms/CommunityManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.swu.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swu/cms/CommunityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swu.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
